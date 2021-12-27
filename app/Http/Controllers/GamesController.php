cfea3187f34f321a90379719a985e840983c5745
Patryk <patrykyrtap@o2.pl>
2021-12-27 11:50:43 +0100

    Get image

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -4,32 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\GamesRequest;
 use App\Models\Game;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Auth;
 
 class GamesController extends Controller
 {
     // GET
-    public function index()
+    public function index(): Response|Application|ResponseFactory
     {
+        $games = Game::where('approve', 1)
+            ->get();
+
+        foreach ($games as $game) {
+            if ($game->image) {
+                $game->image = public_path('images/' . $game->image);
+            }
+        }
+        //        return response()->download(public_path('images/1640292616-Ratchet & Clank.jpg'));
+
         return response([
-            'games' => Game::where('approve', 1)
-                ->get()
+            'games' => $games
         ], 200);
     }
 
     // GET BY ID
     public function show($id)
     {
+        $game = Game::where('id', $id)
+            ->where('approve', 1)
+            ->get();
+
+        if ($game[0]->image) {
+            $game[0]->image = public_path('images/' . $game[0]->image);
+        }
+
         return response([
-            'game' => Game::where('id', $id)
-                ->where('approve', 1)
-                ->get()
+            'game' => $game
         ], 200);
     }
 
     // POST
-    public function store(GamesRequest $request)
+    public function store(GamesRequest $request): Response|Application|ResponseFactory
     {
         $validated = $request->validated();
 
@@ -54,7 +76,7 @@ class GamesController extends Controller
         ], 201);
     }
 
-    public function update(GamesRequest $request, $id)
+    public function update(GamesRequest $request, $id): Response|Redirector|RedirectResponse|Application|ResponseFactory
     {
         $game = Game::findOrFail($id);
 
@@ -85,13 +107,13 @@ class GamesController extends Controller
     }
 
     // VIEWS
-    public function create()
+    public function create(): Factory|View|Application
     {
         return view('games.create');
     }
 
     // temporary view
-    public function welcome()
+    public function welcome(): Factory|View|Application
     {
         $games = Game::get();
         return view('welcome', ['games' => $games]);
